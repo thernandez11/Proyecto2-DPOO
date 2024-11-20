7f23ef8e8afcd800a018f409807093914c3398d9
Angela tatiana <a.barcenast@uniandes.edu.co>
2024-11-19 23:58:38 -0500

    Add methods to guardarEstudiante and cargarEstudiante in PersistenciaEstudiantes

--- Proyecto1DPOO/src/persistencia/PersistenciaEstudiantes.java
+++ Proyecto1DPOO/src/persistencia/PersistenciaEstudiantes.java
@@ -2,4 +2,13 @@ package persistencia;
 
 public class PersistenciaEstudiantes {
 
+
+    public void guardarEstudiante() {
+        // TODO - implement PersistenciaEstudiantes.guardarEstudiante
+    }
+
+    public void cargarEstudiante() {
+        // TODO - implement PersistenciaEstudiantes.cargarEstudiante
+    }
+
 }
